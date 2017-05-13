09c162ff762c79c9f41b27e91ec335951f43fe61
Inno Irving V. Estrera <inno.irving.estrera@gmail.com>
2017-05-13 10:12:49 -0700

    Initial commit

---
+++ src/main/java/IntruderAlertApplication.java
@@ -0,0 +1,15 @@
+/**
+ * CONFIDENTIAL INFORMATION
+ *
+ * All Rights Reserved.  Unauthorized reproduction, transmission, or
+ * distribution of this software is a violation of applicable laws.
+ *
+ * Date: May 13, 2017
+ * Copyright 2017 innoirvinge@gmail.com
+ */
+
+/**
+ * @author irving09 <innoirvinge@gmail.com>
+ */
+public class IntruderAlertApplication {
+}
