dbb6a3733967a586bdd18683660229572b057a9e
Inno Irving V. Estrera <inno.irving.estrera@gmail.com>
2017-05-13 12:05:09 -0700

    Added api key local file

--- src/main/java/IntruderAlertApplication.java
+++ src/main/java/IntruderAlertApplication.java
@@ -8,8 +8,23 @@
  * Copyright 2017 innoirvinge@gmail.com
  */
 
+import com.google.api.services.youtube.YouTube;
+import com.google.common.base.Charsets;
+import com.google.common.io.Files;
+
+import java.io.File;
+import java.io.IOException;
+
 /**
  * @author irving09 <innoirvinge@gmail.com>
  */
 public class IntruderAlertApplication {
+
+  private static YouTube youtube;
+
+  public static void main(String[] args) throws IOException {
+    String apiKey = Files.toString(new File("./src/main/resources/api-key.txt"), Charsets.UTF_8);
+    System.out.println(apiKey);
+  }
+
 }
